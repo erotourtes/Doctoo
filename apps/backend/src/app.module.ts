56c0f73602c76912707ab603c988877b8263e877
juice-artur <asasin.lol10@gmail.com>
2024-04-16 23:14:26 +0300

    feat(backend): Create very basic ```favorite``` entity

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, FavoriteModule],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
---
+++ apps/backend/src/favorite/favorite.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { FavoriteService } from './favorite.service';
+import { CreateFavoriteDto } from './dto/create-favorite.dto';
+import { UpdateFavoriteDto } from './dto/update-favorite.dto';
+
+@Controller('favorite')
+export class FavoriteController {
+  constructor(private readonly favoriteService: FavoriteService) {}
+
+  @Post()
+  create(@Body() createFavoriteDto: CreateFavoriteDto) {
+    return this.favoriteService.create(createFavoriteDto);
+  }
+
+  @Get('by-user/:id')
+  findAllByUserId(@Param('id') id: string) {
+    return this.favoriteService.findAllByPatientId(id);
+  }
+
+  @Delete(':id')
+  deleteFavorite(@Param('id') id: string) {
+    return this.favoriteService.deleteFavorite(id);
+  }
+}
