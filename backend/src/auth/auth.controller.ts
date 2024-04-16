87809be8881c098578d2346c4d5d134d5202e6b2
erotourtes <erotourtes@gmail.com>
2024-04-16 17:45:54 +0300

    feat: added signup controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
 import { GoogleAuthGuard } from 'src/auth/strategies/google-strategy';
 import { LocalAuthGuard } from 'src/auth/strategies/local-strategy';
+import { CreateUserDto } from 'src/user/dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  localLogin(@Request() req) {
+  localLogin(@Request() req, @Body() _: CreateUserDto) {
     return req.user;
+  }
+
+  @Post('signup')
+  localSignup(@Body() createUserDto: CreateUserDto) {
+    return this.authService.signupUser(createUserDto);
   }
 
   @UseGuards(GoogleAuthGuard)
