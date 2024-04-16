857fb7fd2eae3cebcd3ff5a15c6c5a09df0fa57b
LWJerri <loginmanx@gmail.com>
2024-04-16 17:03:42 +0200

    fix(frontend): Fix import path for SVG elements

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,19 +1,19 @@
-import react from "@vitejs/plugin-react"
-import svgr from "vite-plugin-svgr"
-import { defineConfig } from "vitest/config"
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     svgr({
-      include: "*/.svg?react",
+      include: '**/*.svg?react',
     }),
   ],
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests',
     mockReset: true,
   },
-})
+});
