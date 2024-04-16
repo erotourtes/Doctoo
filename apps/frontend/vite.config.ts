7485dcaf4751970f8cbe03459ca6d6887055c828
LWJerri <loginmanx@gmail.com>
2024-04-16 16:04:27 +0200

    feat(frontend): Add new frontend service

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,22 @@
+import react from "@vitejs/plugin-react"
+import svgr from "vite-plugin-svgr"
+import { defineConfig } from "vitest/config"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      include: "*/.svg?react",
+    }),
+  ],
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
+})
