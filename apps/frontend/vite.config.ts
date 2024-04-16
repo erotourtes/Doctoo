9039951dafed5816566e7526b5f5fe8ec299c60a
Iryna Petrenko <112949144+drjbrn@users.noreply.github.com>
2024-04-16 18:34:33 +0300

    feat(frontend): add alias to config (#5)

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,15 @@ export default defineConfig({
       include: '**/*.svg?react',
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@UI': path.resolve(__dirname, './src/components/UI'),
+      '@store': path.resolve(__dirname, './src/store'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
