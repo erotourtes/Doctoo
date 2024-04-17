55e1f9293afa2dc1ce96e07e2aed93ff0c8203c7
HawK <andri0667365973@gmail.com>
2024-04-17 10:05:09 +0300

    Removed changes in readme file

---
+++ apps/frontend/src/components/ui/spinner/spinner.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './spinner.css';
+
+export interface SpinnerProps {
+  size?: number;
+  color?: string;
+}
+
+const Spinner: React.FC<SpinnerProps> = ({ size, color}) => {
+  return (
+    <div className='animate-spin'>
+      <svg
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+       <circle className="spinner spinner_delay_1 w-20 h-20 " cx="12" cy="2" r="3" fill={color} />
+        <circle className="spinner spinner_delay_2" cx="19.071" cy="5.071" r="3" fill={color} />
+        <circle className="spinner spinner_delay_3" cx="21" cy="12" r="3" fill={color} />
+        <circle className="spinner spinner_delay_4" cx="19.071" cy="18.929" r="3" fill={color} />
+        <circle className="spinner spinner_delay_5" cx="12" cy="21" r="3" fill={color} />
+        <circle className="spinner spinner_delay_6" cx="4.929" cy="18.929" r="3" fill={color} />
+        <circle className="spinner spinner_delay_7" cx="3" cy="12" r="3" fill={color} />
+        <circle className="spinner spinner_delay_8" cx="4.929" cy="5.071" r="3" fill={color} />
+      </svg>
+    </div>
+  );
+};
+
+export default Spinner;
