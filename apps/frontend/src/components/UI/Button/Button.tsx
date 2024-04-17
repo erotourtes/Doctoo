3ec4fb3a978a599bf1162fd0d307de97c45957ae
Denys Bolshakov <deni.bolshakov.ua@gmail.com>
2024-04-17 13:44:36 +0300

    feat(frontend): created `Button` component

---
+++ apps/frontend/src/components/UI/Button/Button.tsx
@@ -0,0 +1,19 @@
+import { ButtonTypes } from "./ButtonTypes"
+
+type ButtonProps = {
+    children: React.ReactNode
+    onClick: React.MouseEventHandler<HTMLButtonElement>
+    disabled?: boolean
+    className?: string
+    type: ButtonTypes
+}
+
+export const Button = ({ children, onClick, disabled = false, className = '', type}: ButtonProps) => {
+    const ButtonTypeStyles = {
+        [ButtonTypes.PRIMARY]: `text-white ${disabled ? 'bg-grey-3' :'bg-main hover:bg-main-dark active:bg-main-darker'}`,
+        [ButtonTypes.SECONDARY]: `bg-transparent border-2 ${disabled ? 'border-grey-3 text-grey-3' : 'border-main text-main hover:border-main-dark hover:text-main-dark active:border-main-darker active:text-main-darker'}`
+    }
+  return (
+    <button onClick={onClick} disabled={disabled} className={`${ButtonTypeStyles[type]} h-10 min-w-[100px] px-6 rounded-md ${className}`}>{children}</button>
+  )
+}
