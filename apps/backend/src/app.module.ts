1e1bb6dad6d7c60be5d9437dcd1574bea70974b9
Kolintso Andriy <86800040+HeWHopE@users.noreply.github.com>
2024-04-18 16:14:36 +0300

    Update app.module.ts

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientModule } from './patient/patient.module';
+import { AuthModule } from './auth/auth.module';
+import authConfig from 'src/config/auth-config';
+import config from 'src/config/config';
 import { UserModule } from './user/user.module';
 import { FavoriteModule } from './favorite/favorite.module';
 import { DoctorModule } from './doctor/doctor.module';
@@ -10,7 +13,15 @@ import { AppointmentModule } from './appointment/appointment.module';
 
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, FavoriteModule, DoctorModule, PatientModule, AppointmentModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [authConfig, config] }),
+    UserModule,
+    FavoriteModule,
+    DoctorModule,
+    PatientModule,
+    AuthModule,
+    AppointmentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
