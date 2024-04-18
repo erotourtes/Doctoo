d28ca968d60bfc3395e271526fd4b22142a1a5e7
HawK <andri0667365973@gmail.com>
2024-04-18 15:57:31 +0300

    feat/appointment_entity

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -6,9 +6,11 @@ import { PatientModule } from './patient/patient.module';
 import { UserModule } from './user/user.module';
 import { FavoriteModule } from './favorite/favorite.module';
 import { DoctorModule } from './doctor/doctor.module';
+import { AppointmentModule } from './appointment/appointment.module';
+
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, FavoriteModule, DoctorModule, PatientModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, FavoriteModule, DoctorModule, PatientModule, AppointmentModule],
   controllers: [AppController],
   providers: [AppService],
 })
