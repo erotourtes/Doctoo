e990655d481eb4ea68267c73d76df4966e0ee780
Arseniia-Damaksina <arseniiadamaxcoop@gmail.com>
2024-04-18 15:24:58 +0300

    feat(backend): added docs to patient patch dto

--- apps/backend/src/patient/dto/patch.dto.ts
+++ apps/backend/src/patient/dto/patch.dto.ts
@@ -1,11 +1,14 @@
 import { PartialType } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 import { CreatePatientDto } from './create.dto';
+import { ApiProperty } from '@nestjs/swagger'
 
 export class PatchPatientDto extends PartialType(CreatePatientDto) {
   @IsNumber()
+  @ApiProperty({ description: 'The ID of declaration between the patient and the user' })
   readonly declaration_id: number;
 
   @IsString()
+  @ApiProperty({ description: 'The identity card key of the patient' })
   readonly identity_card_key: string;
 }
