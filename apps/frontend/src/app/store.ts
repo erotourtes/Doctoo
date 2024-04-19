9a0a417015cab18447560349f169d29c0524b635
Ilya Semikashev <tebesski@gmail.com>
2024-04-19 22:10:47 +0300

    fix: Added appointment slice to store

--- apps/frontend/src/app/store.ts
+++ apps/frontend/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { doctorSlice } from './doctor/DoctorSlice';
+import { appointmentSlice } from './appointment/AppointmentSlice';
 
 export const store = configureStore({
   reducer: {
     doctor: doctorSlice.reducer,
+    appointment: appointmentSlice.reducer,
   },
 });
 
