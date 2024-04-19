8488a0c94704647b676e0e014774f7afc68bd27c
Denys Bolshakov <deni.bolshakov.ua@gmail.com>
2024-04-19 21:19:04 +0300

    ci(husky-setup): renamed `checkBranchName.js` to `check-branch-name.js`

--- .husky/hooks/checkBranchName.js
+++
@@ -1,6 +0,0 @@
-const branchName = process.env.HUSKY_GIT_PARAMS.split(' ')[0];
-
-if (/^((fix|hot-fix|main|development|feat|ci)\/[a-zA-Z0-9\-]+)$/gm.test(branchName)) {
-  console.error('Error: Branch name should start with "fix/", "hot-fix/", "main/", "development/", "feat/", "ci/"');
-  process.exit(1);
-}
