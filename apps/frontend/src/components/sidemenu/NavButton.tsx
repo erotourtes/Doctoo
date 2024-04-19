40094129cda57fbaabcf59a4361640796d122978
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-04-19 11:24:47 +0200

    fix: change animation in NavButton

--- apps/frontend/src/components/sidemenu/NavButton.tsx
+++ apps/frontend/src/components/sidemenu/NavButton.tsx
@@ -12,10 +12,10 @@ type NavButtonProps = {
 
 const NavButton: React.FunctionComponent<NavButtonProps> = ({ to, iconVariant, text, variant = 'large', selected = false }) => {
   return (
-    <Link to={to} className={`${variant !== 'small' ? 'w-full flex p-4 rounded-xl' : 'inline-flex p-3 rounded-lg'} relative group gap-2 items-center justify-start ${!selected ? 'text-white' : 'bg-white'} hover:bg-main-dark active:text-white active:bg-main-dark`}>
-      <Icon variant={iconVariant} className={`size-6 group-hover:text-white ${selected ? 'text-main' : ''} transition-all`} />
-      {variant !== 'small' && <span className='font-medium group-hover:text-white'>{text}</span>}
-      {variant === 'small' && <span className='invisible group-hover:visible whitespace-nowrap text-white absolute t-1/2 left-full z-10 ml-1 p-2 bg-main-dark rounded-xl tooltip'>{text}</span>}
+    <Link to={to} className={`${variant !== 'small' ? 'w-full flex p-4 rounded-xl' : 'inline-flex p-3 rounded-lg'} relative group gap-2 items-center justify-start ${!selected ? 'text-white' : 'bg-white'} hover:bg-main-dark active:text-white active:bg-white transition-all`}>
+      <Icon variant={iconVariant} className={`size-6 group-hover:text-white group-active:text-main ${selected ? 'text-main' : ''} transition-all`} />
+      {variant !== 'small' && <span className='font-medium group-hover:text-white group-active:text-black'>{text}</span>}
+      {variant === 'small' && <span className='invisible group-hover:visible whitespace-nowrap text-white absolute t-1/2 left-full z-10 ml-1 p-2 bg-main-dark rounded-xl tooltip transition-all'>{text}</span>}
     </Link>
   )
 }
