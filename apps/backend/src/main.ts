6d4d20f70b1aab7ece4475887df9103b1009677d
erotourtes <erotourtes@gmail.com>
2024-04-21 17:23:40 +0300

    fix: added cookie parser

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   // TODO: Use setGlobalPrefix.
   app.enableCors();
+  app.use(cookieParser());
 
   const configService = app.get<ConfigService>(ConfigService);
 
