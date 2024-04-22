80b72dbc2cab0e8394df60dd66375f0494d8f4fc
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-04-22 10:23:27 +0200

    feat(frontend): Add `Header` component

---
+++ apps/frontend/src/components/UI/Header/Header.tsx
@@ -0,0 +1,16 @@
+import NavLogo from '@UI/NavLogo/NavLogo'
+
+type HeaderProps = {
+  children?: React.ReactNode
+}
+
+const Header: React.FunctionComponent<HeaderProps> = ({ children }) => {
+  return (
+    <header className='w-full flex items-center justify-between bg-main px-8 py-2'>
+      <NavLogo variant='large' />
+      {children}
+    </header>
+  )
+}
+
+export default Header
