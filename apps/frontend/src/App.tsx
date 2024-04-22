905274e11a4ca5f74609e5ac98f3457d39c0a4d4
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-04-22 10:26:25 +0200

    feat(frontend): Update App component to conditionally render Sidemenu or Header

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,32 @@
+import { useLocation } from 'react-router';
 import Sidemenu from '@components/Sidemenu/Sidemenu';
 import PageContainer from './pages/PageContainer';
+import Header from '@UI/Header/Header';
 
 const App = () => {
+  const location = useLocation();
+
+  const shouldDisplaySidemenu = () => {
+    const topLevelPaths = [
+      '/payment',
+      //..
+    ];
+    const currentTopLevelPath = location.pathname.split('/')[1];
+    return !topLevelPaths.includes('/' + currentTopLevelPath);
+  };
+
+  const shouldDispaySmallSideMenu = () => {
+    const topLevelPaths = [
+      '/calendar',
+      //..
+    ];
+    const currentTopLevelPath = location.pathname.split('/')[1];
+    return topLevelPaths.includes('/' + currentTopLevelPath);
+  };
+
   return (
-    <div className='flex h-screen w-screen overflow-hidden'>
-      <Sidemenu />
+    <div className={`flex ${!shouldDisplaySidemenu() ? 'flex-col' : ''} h-screen w-screen overflow-hidden`}>
+      {shouldDisplaySidemenu() ? <Sidemenu variant={shouldDispaySmallSideMenu() ? 'small' : 'large'} /> : <Header />}
       <PageContainer />
     </div>
   );
