c80ee2984dc90811a341930809af544dc1ed4faf
Ilya Semikashev <tebesski@gmail.com>
2024-04-22 10:41:11 +0300

    Fix: deleted unnecessary component

--- apps/frontend/src/components/UI/Popup/PopupHeader.tsx
+++
@@ -1,8 +0,0 @@
-type PopupHeaderProps = {
-  headerClassName: string;
-  children: React.ReactNode;
-};
-
-export default function PopupHeader({ headerClassName, children }: PopupHeaderProps) {
-  return <div className={headerClassName}>{children}</div>;
-}
