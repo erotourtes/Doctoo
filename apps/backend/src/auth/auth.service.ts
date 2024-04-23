dafd7d2dbea6520f6f5fa111513e3ccf83c3d2e9
Andrii Zontov <loginmanx@gmail.com>
2024-04-23 14:03:49 +0200

    fix(backend): Move `identityCardKey` field from POST to  PATCH endpoint (#116)
    * fix(backend): Move `identityCardKey` field from POST to  PATCH endpoint
    
    * fix(backend): Remove `identityCardKey`

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -153,7 +153,6 @@ export class AuthService {
       apartment: body.apartment,
       state: body.state,
       zipCode: body.zipCode,
-      identityCardKey: '',
     });
 
     return plainToInstance(ResponsePatientDto, patient);
