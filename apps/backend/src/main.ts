1b94553a302a69fd5b3dedd26fd5fae4fcd732cb
Denys Bolshakov <deni.bolshakov.ua@gmail.com>
2024-04-23 13:36:31 +0300

    feat(validation-error-formetting): added exceptionFactory for formatting validation error response

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -8,7 +8,19 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: errors => {
+        const formattedErrors = errors.map(error => ({
+          property: error.property,
+          message: error.constraints[Object.keys(error.constraints)[0]],
+        }));
+        return new BadRequestException({ message: 'Validation failed', errors: formattedErrors });
+      },
+    }),
+  );
   // TODO: Use setGlobalPrefix.
   app.enableCors();
   app.use(cookieParser());
