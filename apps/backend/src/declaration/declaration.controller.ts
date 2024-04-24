fb34806506062349a38b503b8824b7573b089bd4
Kolintso Andriy <86800040+HeWHopE@users.noreply.github.com>
2024-04-24 13:05:32 +0300

    fix\ Backend tests and (#134)
    frontend appointment slice

--- apps/backend/src/declaration/declaration.controller.ts
+++ apps/backend/src/declaration/declaration.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { DeclarationService } from './declaration.service';
 import { CreateDeclarationDto } from './dto/create.dto';
 import { UpdateDeclarationDto } from './dto/update.dto';
