fe1c08c9e5af466ed5f16d53c7ad703a8bb68f9d
Iryna Petrenko <iryna.petrenko4@gmail.com>
2024-04-24 13:37:37 +0300

    fix(backend): fix env for payment

--- apps/backend/src/payment/payment.controller.spec.ts
+++ apps/backend/src/payment/payment.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
+import { ConfigService } from '@nestjs/config';
+import { mockConfigService } from '../mocks/config.mock';
 
 describe('PaymentController', () => {
   let controller: PaymentController;
@@ -8,7 +10,13 @@ describe('PaymentController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PaymentController],
-      providers: [PaymentService],
+      providers: [
+        PaymentService,
+        {
+          provide: ConfigService,
+          useValue: mockConfigService,
+        },
+      ],
     }).compile();
 
     controller = module.get<PaymentController>(PaymentController);
--- apps/backend/src/payment/payment.service.spec.ts
+++ apps/backend/src/payment/payment.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentService } from './payment.service';
+import { ConfigService } from '@nestjs/config';
+import { mockConfigService } from '../mocks/config.mock';
 
 describe('PaymentService', () => {
   let service: PaymentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PaymentService],
+      providers: [
+        PaymentService,
+        {
+          provide: ConfigService,
+          useValue: mockConfigService,
+        },
+      ],
     }).compile();
 
     service = module.get<PaymentService>(PaymentService);
