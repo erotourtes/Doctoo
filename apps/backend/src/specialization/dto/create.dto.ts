4f3aba697c48bb0b40e4499dcb36efdfb3b16e2a
LWJerri <loginmanx@gmail.com>
2024-04-25 03:31:08 +0200

    feat(backend): Add basic Swagger docs for `Specialization`

--- apps/backend/src/specialization/dto/create.dto.ts
+++ apps/backend/src/specialization/dto/create.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateSpecializationDto {
+  @ApiProperty({ example: 'Surgeon', description: 'Spesialization name' })
   @IsString({ message: 'name should be a string' })
   readonly name: string;
 }
