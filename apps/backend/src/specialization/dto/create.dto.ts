9d257ba1806cb05ed00b0896cb4423d320f70462
Andrii Zontov <loginmanx@gmail.com>
2024-04-25 09:53:20 +0200

    Update apps/backend/src/specialization/dto/create.dto.ts

--- apps/backend/src/specialization/dto/create.dto.ts
+++ apps/backend/src/specialization/dto/create.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateSpecializationDto {
-  @ApiProperty({ example: 'Surgeon', description: 'Spesialization name' })
+  @ApiProperty({ example: 'Surgeon', description: 'Specialization name' })
   @IsString({ message: 'name should be a string' })
   readonly name: string;
 }
