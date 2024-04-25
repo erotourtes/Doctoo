88a76a5a431baa43afde7303201ccaabf8c25045
Ivan Larin <n.larivanko@gmail.com>
2024-04-25 12:35:40 +0300

    fix: resotre app store to default

--- apps/frontend/src/app/store.ts
+++ apps/frontend/src/app/store.ts
@@ -2,14 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import { doctorSlice } from './doctor/DoctorSlice';
 import { appointmentSlice } from './appointment/AppointmentSlice';
 import { patientSlice } from './patient/PatientSlice';
-import { conditionSlice } from './condition/ConditionSlice';
 
 export const store = configureStore({
   reducer: {
     doctor: doctorSlice.reducer,
     appointment: appointmentSlice.reducer,
     patient: patientSlice.reducer,
-    condtion: conditionSlice.reducer,
   },
 });
 
