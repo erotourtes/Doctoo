fdfa1c07a1e49bc53f58958627aeeedb23df0826
Ivan Larin <n.larivanko@gmail.com>
2024-04-25 10:44:51 +0300

    fix: restore minio ports

--- apps/frontend/src/app/store.ts
+++ apps/frontend/src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { doctorSlice } from './doctor/DoctorSlice';
 import { appointmentSlice } from './appointment/AppointmentSlice';
 import { patientSlice } from './patient/PatientSlice';
+import { conditionSlice } from './condition/ConditionSlice';
 
 export const store = configureStore({
   reducer: {
     doctor: doctorSlice.reducer,
     appointment: appointmentSlice.reducer,
     patient: patientSlice.reducer,
+    condtion: conditionSlice.reducer,
   },
 });
 
