4d1532fffaca82dd76d057a72c161591efe94d64
juice-artur <asasin.lol10@gmail.com>
2024-04-26 21:59:35 +0300

    Added part `Dr.` defore doctor name

--- apps/frontend/src/pages/dashboard/components/DoctorCard/DoctorCard.tsx
+++ apps/frontend/src/pages/dashboard/components/DoctorCard/DoctorCard.tsx
@@ -11,7 +11,7 @@ export default function DoctorCard({ doctor, classNames }: AppointmentCardProps)
       <div className={`${classNames} flex w-2/5 flex-row px-[16px] py-[8px]`}>
         <img src={doctor.avatarKey} alt={doctor.firstName} width='48px' height='48px' className='rounded-lg' />
         <div className='ml-[8px] flex w-[197px] min-w-[197px] flex-col'>
-          <p className='font-semibold leading-6'>{doctor.firstName + ' ' + doctor.lastName}</p>
+          <p className='font-semibold leading-6'>{'Dr. ' + doctor.firstName + ' ' + doctor.lastName}</p>
           <p className='text-sm font-medium leading-5 text-[#707D7E]'>{doctor.about}</p>
         </div>
       </div>
