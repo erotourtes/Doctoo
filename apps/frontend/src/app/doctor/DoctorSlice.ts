64a8aab05179a1a527e8f4fd00f361156949c183
Denys Bolshakov <deni.bolshakov.ua@gmail.com>
2024-04-30 01:07:25 +0300

    updated types on frontend

--- apps/frontend/src/app/doctor/DoctorSlice.ts
+++ apps/frontend/src/app/doctor/DoctorSlice.ts
@@ -2,20 +2,28 @@ import type { RootState } from '@/app/store';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { createAppSlice } from '../createAppSlice';
 import type { IDoctor } from '@/dataTypes/Doctor';
+import type { GetDoctorDataPayload } from './DoctorThunks';
 
 interface DoctorData {
   doctors: IDoctor[];
+  totalCount: number;
 }
 
 const initialState: DoctorData = {
   doctors: [],
+  totalCount: 0,
 };
 
 export const doctorSlice = createAppSlice({
   name: 'doctor',
   initialState,
   reducers: {
-    setDoctorData: (state, action: PayloadAction<IDoctor[]>) => {
+    setDoctorData: (state, action: PayloadAction<GetDoctorDataPayload>) => {
+      state.doctors = action.payload.doctors;
+      state.totalCount = action.payload.count;
+    },
+
+    setPatientDoctorData: (state, action: PayloadAction<IDoctor[]>) => {
       state.doctors = action.payload;
     },
 
@@ -37,7 +45,8 @@ export const doctorSlice = createAppSlice({
   },
 });
 
-export const { setDoctorData, setNewDoctor, setDeleteDoctor, setPatchDoctorData } = doctorSlice.actions;
+export const { setDoctorData, setNewDoctor, setDeleteDoctor, setPatchDoctorData, setPatientDoctorData } =
+  doctorSlice.actions;
 
 export const doctorData = (state: RootState) => state.doctor.doctors;
 
