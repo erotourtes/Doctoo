b58b6dab6731d5712d189aff4dd8faa92cec186c
Max Siryk <67370189+erotourtes@users.noreply.github.com>
2024-04-29 13:24:46 +0300

    Fix/fetch credentials (#231)
    * fix: added credentials `include`

--- apps/frontend/src/api/axios.api.ts
+++ apps/frontend/src/api/axios.api.ts
@@ -2,4 +2,4 @@ import axios from 'axios';
 
 export const API_URL = import.meta.env.VITE_BACKEND_API_URL ?? 'http://localhost:3005';
 
-export const instance = axios.create({ baseURL: API_URL });
+export const instance = axios.create({ baseURL: API_URL, withCredentials: true });
--- apps/frontend/src/app/api.ts
+++ apps/frontend/src/app/api.ts
@@ -3,7 +3,8 @@ import type { paths } from '../api';
 
 const api = createClient<paths>({
   baseUrl: import.meta.env.VITE_BACKEND_API_URL ?? 'http://localhost:3005',
-  credentials: import.meta.env.PROD ? 'same-origin' : 'include',
+  // credentials: import.meta.env.PROD ? 'same-origin' : 'include',
+  credentials: 'include',
 });
 
 export default api;
