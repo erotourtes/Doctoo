9d0ecaeb9778b1e3fa1feb316d3485d98ad6a329
Denys Bolshakov <82179857+d-bolshakov@users.noreply.github.com>
2024-05-01 23:13:28 +0300

    added missing style for cursor pointer on icon (#299)

--- apps/frontend/src/components/UI/Tag/Tag.tsx
+++ apps/frontend/src/components/UI/Tag/Tag.tsx
@@ -18,7 +18,7 @@ const Tag = ({ icon, text, className, onClick }: TagProps) => {
     >
       {text}{' '}
       {icon && (
-        <span className='inline-flex h-6 w-6 items-center justify-center rounded-xl bg-white'>
+        <span className='inline-flex h-6 w-6 cursor-pointer items-center justify-center rounded-xl bg-white'>
           <svg className='' width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
             <path
               fillRule='evenodd'
